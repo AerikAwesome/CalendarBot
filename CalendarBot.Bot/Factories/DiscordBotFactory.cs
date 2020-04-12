51d60377637690ccd5cd5dff3d6e90b7119622d3
Erik Buter <AerikAwesome@users.noreply.github.com>
2020-04-12 13:09:25 +0200

    Add project files.

---
+++ CalendarBot.Bot/Factories/DiscordBotFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalendarBot.Bot.Modules;
+using Disqord;
+using Disqord.Bot;
+using Disqord.Bot.Prefixes;
+
+namespace CalendarBot.Bot.Factories
+{
+    public class DiscordBotFactory
+    {
+        public DiscordBot CreateBot(string token)
+        {
+            var prefixProvider = new DefaultPrefixProvider()
+                .AddPrefix('*')
+                .AddMentionPrefix();
+
+            var bot = new DiscordBot(TokenType.Bot, token, prefixProvider);
+
+            bot.AddModule<PingModule>();
+
+            return bot;
+        }
+    }
+}
---
+++ CalendarBot.Bot/Modules/PingModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Disqord.Bot;
+using Qmmands;
+
+namespace CalendarBot.Bot.Modules
+{
+    public class PingModule : DiscordModuleBase
+    {
+        [Command("ping")]
+        public Task PingAsync() => ReplyAsync("Pong!");
+    }
+}
